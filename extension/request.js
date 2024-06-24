f1515ee6166a6180c72bd862263a46f9d588c8ee
Mark Wylde <me@markwylde.com>
2024-06-24 23:44:03 +0100

    feat: allow using fetch directly, simplifying the code

--- extension/request.js
+++ extension/request.js
@@ -1,15 +1,21 @@
+import syncRules from './syncRules.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   const allowButton = document.querySelector('#allowButton');
   const denyButton = document.querySelector('#denyButton');
 
+  const host = new URLSearchParams(window.location.search).get('host')
+
   allowButton.addEventListener('click', () => {
-    chrome.storage.local.set({['ollamaAuthorized:' + new URLSearchParams(window.location.search).get('origin')]: true}, () => {
+    chrome.storage.local.set({['ollamaAuthorized:' + host]: true}, async () => {
+      await syncRules();
       window.close();
     });
   });
 
   denyButton.addEventListener('click', () => {
-    chrome.storage.local.set({['ollamaAuthorized:' + new URLSearchParams(window.location.search).get('origin')]: false}, () => {
+    chrome.storage.local.set({['ollamaAuthorized:' + host]: false}, async () => {
+      await syncRules();
       window.close();
     });
   });
